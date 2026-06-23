03fa0d6d465aaa14399ca761d8e7e88ed8ae8b1b
Glowlist NYC <ny.coaching.jp@gmail.com>
2026-06-23 16:21:08 -0400

    Fix Google Tag Manager setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import '@/styles/globals.css';
 
 const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL ?? 'https://glowlistnyc.com';
+const GTM_ID = 'GTM-W98ZCH5Z';
 
 export const viewport: Viewport = {
   width: 'device-width',
@@ -36,6 +37,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
+
         {GA_ID && (
           <>
             <Script
@@ -51,7 +62,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           </>
         )}
       </head>
+
       <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+
         <Nav />
         <main>{children}</main>
         <Footer />
